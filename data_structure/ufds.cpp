c5f8dc4cac67849d0a4e9e15c0d008a8494f35b5
alirezakhm <alirezakherad35@gmail.com>
2017-11-01 19:34:09 +0330

    Update ufds.cpp
    set sizes added.

--- data_structure/ufds.cpp
+++ data_structure/ufds.cpp
@@ -1,25 +1,29 @@
-class UnionFind {
-private:
-    vi p, rank; // vi is vector<int>
+class UFDS {
 public:
-    UnionFind(int N) {
-        rank.assign(N, 0);
-        p.assign(N, 0);
-        for (int i = 0; i < N; i++) p[i] = i;
+    vector<int> p, rank, setSizes;
+    int numSets;
+
+    UFDS(int n) {
+        numSets = n;
+        rank.assign(n, 0); p.assign(n, 0);
+        for (int i = 0; i < n; i++) p[i] = i;
+        setSizes.assign(N, 1);
     }
-
-    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
-
+    int findSet(int i) { return (p[i] == i) ? i : p[i] = findSet(p[i]); }
     bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-
     void unionSet(int i, int j) {
-        if (!isSameSet(i, j)) { // if from different set
+        if (!isSameSet(i, j)) {
             int x = findSet(i), y = findSet(j);
-            if (rank[x] > rank[y]) p[y] = x; // rank keeps the tree short
+            if (rank[x] > rank[y]) {
+                setSizes[x] += setSizes[y]; p[y] = x;
+            }
             else {
-                p[x] = y;
+                setSizes[y] += setSizes[x]; p[x] = y;
                 if (rank[x] == rank[y]) rank[y]++;
             }
+            numSets--;
         }
     }
+    int setSize(int i) { return setSizes[findSet(i)]; }
+    void clear(){ p.clear(); rank.clear(); setSizes.clear(); }
 };
