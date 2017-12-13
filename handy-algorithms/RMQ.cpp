bc3e807082ce14b59aec3b4e485bed04ecc1cb7a
mehran <mehranagh200@gmail.com>
2017-12-13 10:18:45 +0330

    Update RMQ alg to return index of min element too

--- handy-algorithms/RMQ.cpp
+++ handy-algorithms/RMQ.cpp
@@ -1,23 +1,27 @@
 struct RMQ { // construct rmq in n * log(n), takes n * log(n) space. query in O(1).
     int len;
     vvi rmq;
+    vvi ind;
 
     void init(vi arr) { // call this method with your desired array.
         len = arr.size();
         int lg = log(len) / log(2) + 20;
         rmq.assign(len + 10, vi(lg, 0));
+        ind.assign(len + 10, vi(lg, 0));
         for (int i = 0; i < len; i++)
-            rmq[i][0] = arr[i];
+            rmq[i][0] = arr[i], ind[i][0] = i;
         for (int j = 1; j < lg; j++)
             for (int i = 0; i < len; i++) {
                 if (i + (1 << j) > len)
                     break;
-                rmq[i][j] = rmq[i][j - 1];
-                rmq[i][j] = min(rmq[i][j - 1], rmq[i + (1 << (j - 1))][j-1]);
+                if(rmq[i][j - 1] < rmq[i + (1 << (j - 1))][j - 1])
+                    rmq[i][j] = rmq[i][j - 1], ind[i][j] = ind[i][j - 1];
+                else
+                    rmq[i][j] = rmq[i + (1 << (j - 1))][j - 1], ind[i][j] = ind[i + (1 << (j - 1))][j - 1];
             }
     }
 
-    int range_minimum_query(int l, int r) { // it is 0 base, manage so that 'l' and 'r' are in range!
+    ii range_minimum_query(int l, int r) { // it is 0 base, manage so that 'l' and 'r' are in range!, first element is the min element, second element is index of min element in the range.
         if (l > r)
             swap(l, r);
 
@@ -29,6 +33,8 @@ struct RMQ { // construct rmq in n * log(n), takes n * log(n) space. query in O(
         first_half--;
 
         int second_half = r - (1 << first_half) + 1;
-        return min(rmq[l][first_half], rmq[second_half][first_half]);
+        if(rmq[l][first_half] < rmq[second_half][first_half]) return ii(rmq[l][first_half], ind[l][first_half]);
+        else return ii(rmq[second_half][first_half], ind[second_half][first_half]);
+
     }
 };
