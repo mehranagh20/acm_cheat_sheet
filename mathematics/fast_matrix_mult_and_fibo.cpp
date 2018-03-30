28e480416ea62169adb2d769484151416f265f03
Mehran <mehranagh200@gmail.com>
2018-03-30 18:46:59 +0430

    Improve matrix pow and multiplicationi to be more natural :)

--- mathematics/fast_matrix_mult_and_fibo.cpp
+++ mathematics/fast_matrix_mult_and_fibo.cpp
@@ -1,31 +1,30 @@
-#define MAX_N 2                                  // increase this if needed
-struct Matrix { ll mat[MAX_N][MAX_N]; };     // to let us return a 2D array
+ll n = 3, MOD = 1e9 + 7; // change n and MOD to meet your requirements
 
-Matrix matMul(Matrix a, Matrix b) {            // O(n^3), but O(1) as n = 2
-  Matrix ans; int i, j, k;
-  for (i = 0; i < MAX_N; i++)
-    for (j = 0; j < MAX_N; j++)
-      for (ans.mat[i][j] = k = 0; k < MAX_N; k++) {
-        ans.mat[i][j] += (a.mat[i][k] % MOD) * (b.mat[k][j] % MOD);
-        ans.mat[i][j] %= MOD;             // modulo arithmetic is used here
-      }
-  return ans;
+vvi matMul(vvi &a, vvi &b) {
+    vvi c(n, vi(n, 0));
+    int k;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (k = 0; k < n; k++) {
+                c[i][j] += (a[i][k] % MOD) * (b[k][j] % MOD);
+                c[i][j] %= MOD;
+            }
+    return c;
 }
 
-Matrix matPow(Matrix base, int p) {  // O(n^3 log p), but O(log p) as n = 2
-  Matrix ans; int i, j;
-  for (i = 0; i < MAX_N; i++)
-    for (j = 0; j < MAX_N; j++)
-      ans.mat[i][j] = (i == j);                  // prepare identity matrix
-  while (p) {       // iterative version of Divide & Conquer exponentiation
-    if (p & 1)                    // check if p is odd (the last bit is on)
-      ans = matMul(ans, base);                                // update ans
-    base = matMul(base, base);                           // square the base
-    p >>= 1;                                               // divide p by 2
-  }
-  return ans;
+vvi matPow(vvi base, ll p) {
+    vvi ans(n, vi(n, 0)); int i, j;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            ans[i][j] = (i == j);
+    while (p) {
+        if (p & 1)
+            ans = matMul(ans, base);
+        base = matMul(base, base);
+        p >>= 1;
+    }
+    return ans;
 }
-
 
 [[1 , 1] , [1 , 0]] ** n = [[fib(n+1) , fib(n)], [fib(n) , fib(n-1)]] 
 // FIBONACCI FORMULA
