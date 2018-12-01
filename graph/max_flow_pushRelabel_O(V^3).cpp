f0266ed3586c3c8f9b042b6a81d7b99cbd934d68
alirezakhm <alirezakherad35@gmail.com>
2018-12-01 21:27:02 +0330

    Create max_flow_pushRelabel_O(V^3).cpp

---
+++ graph/max_flow_pushRelabel_O(V^3).cpp
@@ -0,0 +1,64 @@
+// O(VE + V^2√E) , worst case is O(V3)
+
+int n;
+vvi capacity, flow;
+vi height, excess;
+
+void push(int u, int v){
+    auto d = min(excess[u], capacity[u][v] - flow[u][v]);
+    flow[u][v] += d, flow[v][u] -= d;
+    excess[u] -= d, excess[v] += d;
+}
+
+void relabel(int u){
+    int d = inf;
+    forn(i, n)
+        if (capacity[u][i] - flow[u][i] > 0)
+            d = min(d, height[i]);
+    
+    if (d < inf) height[u] = d + 1;
+}
+
+vi find_max_height_vertices(int s, int t) {
+    vi max_height;
+    forn(i, n) {
+        if (i != s && i != t && excess[i] > 0) {
+            if (!max_height.empty() && height[i] > height[max_height[0]])
+                max_height.clear();
+            if (max_height.empty() || height[i] == height[max_height[0]])
+                max_height.push_back(i);
+        }
+    }
+    return max_height;
+}
+
+int max_flow(int s, int t)
+{
+    height.assign(n, 0); height[s] = n;
+    flow.assign(n, vi(n, 0));
+    excess.assign(n, 0); excess[s] = inf;
+    
+    forn(i, n) if (i != s) push(s, i);
+
+    vi current;
+    while (!(current = find_max_height_vertices(s, t)).empty()) {
+        for(auto i : current) {
+            bool pushed = false;
+            for (int j = 0; j < n && excess[i]; j++) {
+                if (capacity[i][j] - flow[i][j] > 0 && height[i] == height[j] + 1) {
+                    push(i, j);
+                    pushed = true;
+                }
+            }
+            if (!pushed) {
+                relabel(i);
+                break;
+            }
+        }
+    }
+
+    int max_flow = 0;
+    for (int i = 0; i < n; i++)
+        max_flow += flow[0][i];
+    return max_flow;
+}
