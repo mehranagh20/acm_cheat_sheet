edbaa41a2647e3d98b4e942faab1cc1033b504b4
alirezakhm <alirezakherad35@gmail.com>
2018-12-11 09:25:02 +0330

    Update max-bipartite-matching-O(EV).cpp

--- graph/max-bipartite-matching-O(EV).cpp
+++ graph/max-bipartite-matching-O(EV).cpp
@@ -1,28 +1,22 @@
-vi match, vis;
-// global variables
+vi match, vis; // global variables
 int Aug(int l) {
-// return 1 if an augmenting path is found
+    // return 1 if an augmenting path is found, 0 otherwise
     if (vis[l]) return 0;
-// return 0 otherwise
+
     vis[l] = 1;
-    for (int j = 0; j < (int)AdjList[l].size(); j++) {
-        int r = AdjList[l][j]; // edge weight not needed -> vector<vi> AdjList
-        if (match[r] == -1 || Aug(match[r])) {
-            match[r] = l; return 1;
-// found 1 matching
+    for(auto r: AdjList[l]) {
+        if (match[r] == -1 || Aug(match[r])) { // found 1 matching
+            match[r] = l; return 1;         
         } }
-    return 0;
-// no matching
+    return 0; // no matching
 }
-// inside int main()
+
+// inside main
 // build unweighted bipartite graph with directed edge left->right set
+// V is the number of vertices in bipartite graph
+// n = size of the left set
+
 int MCBM = 0;
 match.assign(V, -1);
-// V is the number of vertices in bipartite graph
-for (int l = 0; l < n; l++) {
-// n = size of the left set
-vis.assign(n, 0);
-// reset before each recursion
-MCBM += Aug(l);
-}
+forn(l, n) { vis.assign(n, 0); MCBM += Aug(l); }
 printf("Found %d matchings\n", MCBM);
