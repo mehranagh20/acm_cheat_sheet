02e8a807de9c14ccb1dc2c3d6650f1bd9ae4cd58
alirezakhm <alirezakherad35@gmail.com>
2018-12-12 21:14:00 +0330

    Update topological_sort_kahs's_O(V+E).cpp

--- graph/topological_sort_kahs's_O(V+E).cpp
+++ graph/topological_sort_kahs's_O(V+E).cpp
@@ -1,23 +1,16 @@
-// kahn's alg for finding a valid topological sort.
-// in TopSort 'u' comes before 'v' if edge u -> v exists in DAG. 
+// n = # of vertexes. indegree stores number of incoming edges to i'th vertex.
+// fill n and calculate indegree before running alg. ts holds the Topsort
 
-// n is number of vertexes. indegree stores number of incoming edges to i'th vertex.
-// fill n and indegree before defination of priority_queue.
-// ts holds the Topsort
+vi indegree(n, 0), vis(n, 0), ts;
 
-vi indegree(n, 0), vis(n, 0);
-//calculate indegree before running alg.
-
-vi ts;
 priority_queue<int, vi, greater<int>> pq; 
-for(int i = 0; i < n; i++) if(!indegree[i]) pq.push(i);
-        while(!pq.empty()) {
-            int top = pq.top(); pq.pop();
-            vis[top] = 1;
-            ts.push_back(top);
-            for(auto &e : graph[top]) {
-                if(vis[e]) continue;
-                indegree[e]--;
-                if(!indegree[e]) pq.push(e);
-            }
-        }
+forn(i, n) if(!indegree[i]) pq.push(i);
+while(!pq.empty()) {
+    int top = pq.top(); pq.pop();
+    vis[top] = 1; ts.push_back(top);
+    for(auto &e : graph[top]) {
+        if(vis[e]) continue;
+        indegree[e]--;
+        if(!indegree[e]) pq.push(e);
+    }
+}
