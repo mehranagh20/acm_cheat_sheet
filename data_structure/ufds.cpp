1a86246ab87b2d926e63956b57b2b98d8ae59f1f
alirezakhm <alirezakherad35@gmail.com>
2018-12-13 14:38:53 +0330

    Update ufds.cpp

--- data_structure/ufds.cpp
+++ data_structure/ufds.cpp
@@ -1,13 +1,13 @@
 class UFDS {
 public:
-    vector<int> p, rank, setSizes;
+    vi p, rank, setSizes;
     int numSets;
 
     UFDS(int n) {
         numSets = n;
         rank.assign(n, 0); p.assign(n, 0);
-        for (int i = 0; i < n; i++) p[i] = i;
-        setSizes.assign(N, 1);
+        forn(i, n) p[i] = i;
+        setSizes.assign(n, 1);
     }
     int findSet(int i) { return (p[i] == i) ? i : p[i] = findSet(p[i]); }
     bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
@@ -25,5 +25,4 @@ public:
         }
     }
     int setSize(int i) { return setSizes[findSet(i)]; }
-    void clear(){ p.clear(); rank.clear(); setSizes.clear(); }
 };
