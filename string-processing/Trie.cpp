e65ad8231c493ea732dd6bc117c029d566b81943
alirezakhm <alirezakherad35@gmail.com>
2018-12-15 01:58:42 +0330

    Create Trie.cpp

---
+++ string-processing/Trie.cpp
@@ -0,0 +1,37 @@
+struct trie {
+    bool isWord;
+    map<char, trie*> children;
+    trie(): isWord(false){}
+};
+
+void trieInsert(trie *&root, string &str){
+    trie* cur = root;
+    for(auto &e: str){
+        if(!cur->children.count(e)) cur->children[e] = new trie;
+        cur = cur->children[e];
+    }
+    cur->isWord = true;
+}
+
+bool trieSearch(trie *&root, string &str){
+    trie* cur = root;
+    for(auto &e: str){
+        if(!cur->children.count(e)) return false;
+        cur = cur->children[e];
+    }
+
+    return cur->isWord;
+}
+
+// call with root. search for str before deletion
+void trieDelete(trie *cur, string &str, int ind){
+    trie *nxt = cur->children[str[ind]];
+    if(ind == str.size() - 1){
+        nxt->isWord = false; return;
+    }
+    trieDelete(nxt, str, ind + 1);
+
+    if(nxt->children.empty()) {
+        delete nxt; cur->children.erase(str[ind]);
+    }
+}
