0c125e2b45c6ef9c3ad25dc5cabfd3bfcb680055
Mehran <merhanagh200@gmail.com>
2019-01-31 15:53:22 +0330

    Add some typedef to template

--- template.cpp
+++ template.cpp
@@ -19,13 +19,16 @@ typedef vector<ii> vii;
 typedef vector<vii> vvii;
 
 #define forn(i, n) for (int i = 0; i < n; i++)
+#define forr(i, n) for (int i = n; i >= 0; i--)
+#define forne(i, x, n) for (int i = x; i < n; i++)
+#define forre(i, x, n) for (int i = n; i >= x; i--)
 #define all(a) (a).begin(), (a).end()
 #define inf 1000000000
 #define eps 1e-9
 
 int main() {
     ios::sync_with_stdio(0);
-
-
+    
     return 0;
 }
+
